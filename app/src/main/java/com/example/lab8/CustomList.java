4d0839966ab3f03369ad6c075051b300734ede67
mhshadab <shadab@ualberta.ca>
2023-03-10 13:51:10 -0700

    Has city test will pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -62,8 +62,12 @@ public class CustomList extends ArrayAdapter<City> {
 
 
     public short hasCity(City city){
-
-        return 0;
+        if(cities.contains(city)){
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
 
 }
