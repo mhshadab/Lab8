07af95eb40396a4d4101ea803a7da47dca6059ba
mhshadab <shadab@ualberta.ca>
2023-03-10 14:01:05 -0700

    Delete city will pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -71,6 +71,13 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void delete(City city) {
+        if (cities.contains(city)) {
+            cities.remove(city);
+
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
 
     }
 
